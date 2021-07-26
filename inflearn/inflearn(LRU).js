befb02616da43a153e8a5f55c566294a2d57fa51
RyuJungSik <76714485+RyuJungSik@users.noreply.github.com>
2021-07-26 09:39:08 +0900

    0726

---
+++ inflearn/inflearn(LRU).js
@@ -0,0 +1,30 @@
+
+function solution(size, arr) {
+    const S=size;
+    const N=arr.length;
+    let answer=[]
+
+    arr.forEach(element => {
+        let tmp;
+        if(!answer.includes(element)){
+            if(answer.length===S){
+                answer.shift()
+            }
+            answer.push(element)
+        }else{
+            answer.forEach((element2, index)=>{
+                if(element2===element){
+                    tmp=index
+                }
+            })
+            answer.splice(tmp, 1)
+            answer.push(element)
+        }
+    });
+
+    
+    return answer.reverse();
+}
+
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));

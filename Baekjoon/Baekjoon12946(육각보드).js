8a33c39ccf607bb4122c647edf919ecf3ac4a454
RyuJungSik <xvbc123@gmail.com>
2021-12-29 10:29:45 +0900

    Add : Baekjoon 1229

--- Baekjoon/Baekjoon12946(육각보드).js
+++ Baekjoon/Baekjoon12946(육각보드).js
@@ -1,63 +1,45 @@
-const { BADFLAGS } = require("dns");
 const fs = require("fs");
-const { start } = require("repl");
-const { deflateSync } = require("zlib");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let n = +input.shift();
-input = input.map((str) => str.split(""));
-for (let i = 0; i < input.length - 1; i++) input[i].pop();
-const solution = (n, input) => {
-  let check = Array.from({ length: n }, () => Array(n).fill(0));
+
+const solution = (input) => {
+  let n = parseInt(input[0]);
+  let a = input.slice(1, n + 1).map((v) => v.split(""));
+  console.log(a);
+  let check = Array.from({ length: n }, () => Array(n).fill(-1));
   const [dx, dy] = [
     [-1, -1, 0, 0, 1, 1],
     [0, 1, -1, 1, -1, 0],
   ];
   let answer = 0;
-  const dfs = (x, y) => {
+  const dfs = (x, y, c) => {
+    check[x][y] = c;
+    answer = Math.max(answer, 1);
     for (let d = 0; d < 6; d++) {
-      let [nx, ny] = [x + dx[d], y + dy[d]];
-      if (
-        nx < 0 ||
-        nx >= n ||
-        ny < 0 ||
-        ny >= n ||
-        check[nx][ny] !== 0 ||
-        input[nx][ny] === "-"
-      )
-        continue;
-
-      const hubo = new Array(8).fill(false);
-      for (let i = 0; i < 6; i++) {
-        let [nnx, nny] = [nx + dx[i], ny + dy[i]];
-        if (nnx < 0 || nnx >= n || nny < 0 || nny >= n) continue;
-        hubo[check[nnx][nny]] = true;
-      }
-      for (let i = 1; i < 8; i++) {
-        if (hubo[i] === false) {
-          check[nx][ny] = i;
-          break;
+      const [nx, ny] = [x + dx[d], y + dy[d]];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+        if (a[nx][ny] === "X") {
+          if (check[nx][ny] === -1) {
+            dfs(nx, ny, 1 - c);
+            answer = Math.max(answer, 2);
+          } else if (check[nx][ny] === c) {
+            answer = Math.max(answer, 3);
+            return;
+          }
         }
       }
-      dfs(nx, ny);
     }
   };
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (input[i][j] === "-" || check[i][j] !== 0) continue;
-      check[i][j] = 1;
-      dfs(i, j);
+      if (a[i][j] === "X" && check[i][j] === -1) {
+        dfs(i, j, 0);
+      }
     }
   }
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (answer < check[i][j]) answer = check[i][j];
-    }
-  }
-  console.log(check);
   console.log(answer);
 };
 
-solution(n, input);
+solution(input);
